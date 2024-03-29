1697f6f75c9be44714dfa8da4c8e4a4ef7b16a9a
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-29 15:33:54 +0530

    updated

--- react02/src/Mobile.js
+++ react02/src/Mobile.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import MobileList from './MobileList'
-
-const book1 = {
-     image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/z/4/r/-original-imagtc4g22rcatjg.jpeg?q=70',
-     title: 'Apple iPhone 15 Pro',
-     price: '₹129999'
-}
+import mobiles from './mobiles.json'
 
 export default function Mobile(){
+    
   return (
     <div>
-        <MobileList image={book1.image} title={book1.title} price={book1.price}/>
+
+        {mobiles.map((element)=>{
+            return <MobileList 
+                    image={element.image} 
+                    title={element.title} 
+                    price={element.price}
+                    />
+        })}
+     
     </div>
   )
 }
-
--- react02/src/MobileList.js
+++ react02/src/MobileList.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './MobileList.css'
 
+
 export default function MobileList(props){
+
+    const {image,title,price} = props; //de-structuring
 
   return (
     <div className='main'>
         {console.log(props)}
-        <img className='image' src={props.image} alt='iphone'/>
+        <img className='image' src={image} alt='iphone'/>
         <div>
-            <h2>{props.title}</h2>
-            <p>{props.price}</p>
+            <h2>{title}</h2>
+            <p>{price}</p>
             <button>Add to Cart</button>
         </div>
 
