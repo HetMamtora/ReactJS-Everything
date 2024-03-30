e95b55670cae70b7d1b5d81633144e13d44f8116
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-30 16:14:47 +0530

    Update UseEffect.js

--- react03/src/UseEffect.js
+++ react03/src/UseEffect.js
@@ -1,9 +1,24 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 
 const UseEffect = () => {
+
+  const [clicked,setClicked] = useState();
+  const [count,setCount] = useState(0);
+
+  useEffect(()=>{
+    console.log('Inside useState');
+  },[clicked,count]);
+
   return (
     <div>
         <h1>Use Effect Eg</h1>
+        <button onClick={()=>setClicked('Click')}>Click</button> <br /> <hr />
+        <button onClick={()=>setClicked('For')}>For</button> <br /> <hr />
+        <button onClick={()=>setClicked('Surprise')}>Surprise</button> <br />
+        <h1>{clicked}</h1>
+
+        <button onClick={()=>{setCount(count+1)}}>Increment</button>
+        <h1>{count}</h1>
     </div>
   )
 }
