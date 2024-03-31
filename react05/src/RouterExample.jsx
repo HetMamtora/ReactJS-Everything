24aa37cad54f96d242f789c39e002d32afd7bb4d
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-31 15:58:53 +0530

    updated

---
+++ react05/src/RouterExample.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom';
+
+const RouterExample = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default RouterExample
